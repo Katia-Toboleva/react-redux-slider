37e12aa3fe459381dc1be152a6333d32329a5eb6
Katia-2020 <kateflights@gmail.com>
2020-12-16 21:28:52 +0000

    added app.jsx

---
+++ client/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from './components/slider';
+
+import './reset.scss';
+
+const App = () => (
+  <div className="app">
+    <Slider />
+  </div>
+);
+
+export default App;
